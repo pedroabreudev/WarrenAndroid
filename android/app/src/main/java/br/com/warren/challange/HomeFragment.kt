1af96557524820d7c99e1c612f6569c05442f6ad
Pedro Henrique <pedroabreu.dev@gmail.com>
2021-12-31 17:47:07 -0400

    Completed HomeFragment

--- android/app/src/main/java/br/com/warren/challange/HomeFragment.kt
+++ android/app/src/main/java/br/com/warren/challange/HomeFragment.kt
@@ -1,59 +1,33 @@
 package br.com.warren.challange
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import android.widget.Toast
+import androidx.fragment.app.Fragment
+import br.com.warren.challange.databinding.FragmentHomeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    private lateinit var binding: FragmentHomeBinding
+    private val imagens =
+        intArrayOf(R.drawable.home, R.drawable.conta, R.drawable.carteiras, R.drawable.trade)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHomeBinding.bind(view)
+
+        binding.carouselView.pageCount = imagens.size
+        binding.carouselView.setImageListener { position, imageView ->
+            imageView.setImageResource(imagens[position])
         }
-    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
-    }
+        binding.btnOpenAccount.setOnClickListener {
+            Toast.makeText(context, "Função indisponível no momento!", Toast.LENGTH_SHORT).show()
+        }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        binding.btnLogin.setOnClickListener {
+            Toast.makeText(context, "Função indisponível no momento!", Toast.LENGTH_SHORT).show()
+        }
     }
 }
--- android/app/src/main/java/br/com/warren/challange/MainActivity.kt
+++ android/app/src/main/java/br/com/warren/challange/MainActivity.kt
@@ -2,15 +2,22 @@ package br.com.warren.challange
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
 import br.com.warren.challange.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navController: NavController
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHostFragment.navController
     }
 }
