2806a121f6a49cc8d0d8466af6856b81c885e9cc
Pedro Henrique <pedroabreu.dev@gmail.com>
2022-01-04 10:35:36 -0400

    Created BaseFragment and ViewModelFactory

---
+++ android/app/src/main/java/br/com/warren/challange/ui/base/BaseFragment.kt
@@ -0,0 +1,33 @@
+package br.com.warren.challange.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<VM : ViewModel, VB : ViewBinding> : Fragment() {
+
+    private lateinit var binding: VB
+    private lateinit var viewModel: VM
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = getFragmentBinding(inflater, container)
+        val factory = ViewModelFactory(getFragmentRepository())
+        viewModel = ViewModelProvider(this, factory).get(getViewModel())
+        return binding.root
+    }
+
+    abstract fun getFragmentRepository(): Any
+
+    abstract fun getViewModel(): Class<VM>
+
+    abstract fun getFragmentBinding(inflater: LayoutInflater, container: ViewGroup?): VB
+}
---
+++ android/app/src/main/java/br/com/warren/challange/ui/base/ViewModelFactory.kt
@@ -0,0 +1,7 @@
+package br.com.warren.challange.ui.base
+
+import androidx.lifecycle.ViewModelProvider
+
+class ViewModelFactory(fragmentRepository: Any) : ViewModelProvider.NewInstanceFactory() {
+
+}
