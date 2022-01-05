5c8fa8058cb378fa40efeab89cb0162442fb35f7
Pedro Henrique <pedroabreu.dev@gmail.com>
2022-01-04 21:06:06 -0400

    Created xml fragment_objectives_list, item_objective, fragment ObjectivesListFragment

--- android/app/src/main/java/br/com/warren/challange/ui/objective/ObjectivesListFragment.kt
+++ android/app/src/main/java/br/com/warren/challange/ui/objective/ObjectivesListFragment.kt
@@ -1,8 +1,19 @@
 package br.com.warren.challange.ui.objective
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import br.com.warren.challange.R
+import br.com.warren.challange.databinding.FragmentObjectivesListBinding
 
 
-class ObjectivesListFragment : Fragment() {
+class ObjectivesListFragment : Fragment(R.layout.fragment_objectives_list) {
+
+    private lateinit var binding: FragmentObjectivesListBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentObjectivesListBinding.bind(view)
+    }
 
 }
